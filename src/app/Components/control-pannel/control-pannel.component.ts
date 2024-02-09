58e04e43c55ccaf4e55c0803eec9fd115a28dfae
samuelepecci <samuelepecci99@gmail.com>
2024-02-09 12:56:28 +0100

    Fine implementazione pannello controllo e miglioramenti grafici

--- src/app/Components/control-pannel/control-pannel.component.ts
+++ src/app/Components/control-pannel/control-pannel.component.ts
@@ -21,9 +21,40 @@ export class ControlPannelComponent {
   
   constructor(private sys:SystemService, private prodotti:ProdottiService)
   {
-    this.credito = sys.getCredito();
-    this.incasso = sys.getIncasso();
+    const multiplier = Math.pow(10, 2);
+    this.credito = Math.round(sys.getCredito() * multiplier) / multiplier;
+    this.incasso = Math.round(sys.getIncasso() * multiplier) / multiplier
+
     this.bevandeCalde = prodotti.getBevandeCalde();
     this.prodottiFreddi = prodotti.getProdottiFreddi();
+  }
+
+  creaCsv() : void
+  {
+    let data:String[][] = [];
+
+    data.push(["Pannello di controllo"])
+    data.push(["Credito", this.credito.toString()], ["Incasso", this.incasso.toString()])
+    
+    data.push([],["Prodotti Caldi","Q.ta Venduta", "Q.ta disponibile"]);
+    this.bevandeCalde.forEach(e => {
+      data.push([e.nome, e.qtaVenduta.toString(), e.qtaDisponibile.toString()]);
+    });
+
+    data.push([],["Prodotti Freddi","Q.ta Venduta", "Q.ta disponibile"]);
+    this.prodottiFreddi.forEach(e => {
+      data.push([e.nome, e.qtaVenduta.toString(), e.qtaDisponibile.toString()]);
+    });
+
+    let contenuto = data.map(row => row.join(';')).join('\n');
+    let blob = new Blob([contenuto], { type: 'csv'})
+    let url = URL.createObjectURL(blob);
+    let link = (document.createElement('a'));
+    
+    link.href = url;
+    link.download = "PannelloDiControllo.csv";
+    link.click();
+
+    URL.revokeObjectURL(url);
   }
 }
